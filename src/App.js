10ee44b736d9cc8fbabe8cdc6a944a174d3b3ef7
tamsue-crypto <mateusbarros45@yahoo.com>
2025-11-24 19:45:25 -0300

    Deployed Website

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   
   return (
     <>
-      <Router>
+      <Router basename="/Refscore">
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<HomePage/>}/>
